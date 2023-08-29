3e9fd27b478327d3084c41418a0f5f204ad78a91
khushijoshiii <139934070+khushijoshiii@users.noreply.github.com>
2023-08-29 16:59:50 +0530

    Add files via upload

---
+++ typing speed test.py
@@ -0,0 +1,31 @@
+from time import*
+import random as r
+
+def Mistake(partest,usertest):
+	error=0
+	for i in range(len(partest)):
+		try:
+			if partest[i] !=usertest[i]:
+			    error=error+1
+		except:
+			error=error+1
+	return error
+def speed_time(time_s,time_e,userinput):
+	time_delay=time_e-time_s
+	time_R= round(time_delay,2)
+	speed=len(userinput)/time_R
+	return round(speed)
+
+test=["A paragraph is a self contained unit of discourse in writing dealing with a particular point or idea.",
+"My name is khushi joshi","welcome to my project"]
+test1=r.choice(test)
+print("*****typing speed*****")
+print(test1)
+print()
+print()
+time_1= time()
+testinput=input("Enter:")
+time_2= time()
+
+print('Speed:',speed_time(time_1,time_2,testinput),"w/sec")
+print("Error:",Mistake(test1,testinput))
